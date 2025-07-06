268e1b17331329c2bffd023282536e83b03b2457
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-07-06 09:14:46 +0900

    feat: add TypeScript command implementation

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
 import { Command } from "@commander-js/extra-typings";
-import { makeSecurityCommand } from "./commands";
+import { makeSecurityCommand, makeTypescriptCommand } from "./commands";
 
 const program = new Command();
 program.addCommand(makeSecurityCommand());
+program.addCommand(makeTypescriptCommand());
 
 program.parse();
